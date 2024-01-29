100ee76b335b6e9922d5b1ffb6350341d6a2a060
applepieorchard <paul.miclaus1229@gmail.com>
2024-01-29 03:08:18 +0000

    Created an onclick event for the search button and moved entire code so far into it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
-const geoLimit = 5;
-var lat = 0;
-var lon = 0;
-var search = "London";
-const apiKey = "7acd9fd35411ccb740fad8f5750d8c5d";
-const geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=${geoLimit}&appid=${apiKey}`;
+$("#search-button").on("click", function(event) {
+  event.preventDefault()
 
-fetch(geoQuery)
-  .then(function(geoRes) {
-    return geoRes.json()
-  }).then(function(geo) {
-    var lat = geo[0].lat;
-    var lon = geo[0].lon;
-    const weatherQuery = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const geoLimit = 1;
+  const search = $("#search-input").val();
+  const apiKey = "7acd9fd35411ccb740fad8f5750d8c5d";
+  const geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=${geoLimit}&appid=${apiKey}`;
     
-    fetch(weatherQuery)
-      .then(function(res) {
-        return res.json()
-      }).then(function(data) {
-        console.log(data)
-      })
-  })
+  fetch(geoQuery)
+    .then(function(geoRes) {
+      return geoRes.json()
+    }).then(function(geo) {
+      const lat = geo[0].lat;
+      const lon = geo[0].lon;
+      const weatherQuery = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+      
+      fetch(weatherQuery)
+        .then(function(res) {
+          return res.json()
+        }).then(function(data) {
+          console.log(data)
+        })
+    })
+})
