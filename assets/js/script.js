99ce99bfe77e99a87797e6cf1690ee23da28c71f
applepieorchard <paul.miclaus1229@gmail.com>
2024-01-29 06:31:32 +0000

    Added output for today's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,59 @@ $("#search-button").on("click", function(event) {
         .then(function(res) {
           return res.json()
         }).then(function(data) {
-          console.log(data)
+          console.log(data);
+
+          var forecastEl = $("#today");
+          forecastEl.empty();
+          
+            var cityEl = $("<section>");
+            cityEl.attr("id", "city");
+            
+              const cityName = data.city.name;
+              const date = (data.list[2].dt + data.city.timezone) * 1000;
+              var dateFormat = (dayjs(date).format("DD/MM/YYYY"))
+              var headerEl = $("<h3>");
+
+              headerEl.text(`${cityName} (${dateFormat})`);
+              headerEl.attr("id", "city-header");
+
+              const iconKey = data.list[2].weather[0].icon;
+              const weatherIcon = `http://openweathermap.org/img/w/${iconKey}.png`;
+              var iconEl = $("<img>")
+
+              iconEl.attr("src", weatherIcon);
+              iconEl.attr("alt", "Weather Icon");
+              iconEl.addClass("weather-icon");
+
+            cityEl.append(headerEl);
+            cityEl.append(iconEl);
+                      
+            var weatherEl = $("<section>");
+
+              var tempEl = $("<p>");
+              const temperature = data.list[2].main.temp - 273.15;
+
+              tempEl.text(`Temp: ${Math.floor(temperature)}°C`);
+              tempEl.addClass("temp");
+              
+              var humidityEl = $("<p>");
+              const humidity = data.list[2].main.humidity;
+
+              humidityEl.text(`Humidity: 💧${humidity}%`);
+              humidityEl.addClass("humidity");
+              
+              var windEl = $("<p>");
+              const windSpeed = data.list[2].wind.speed
+              
+              windEl.text(`Wind: ${windSpeed}km/h`)
+              windEl.addClass("wind-speed")
+
+            weatherEl.append(tempEl);
+            weatherEl.append(humidityEl);
+            weatherEl.append(windEl);
+
+          forecastEl.append(cityEl);
+          forecastEl.append(weatherEl);
         })
     })
 })
